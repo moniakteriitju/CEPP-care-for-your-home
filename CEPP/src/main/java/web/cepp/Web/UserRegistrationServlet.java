11b2951af6dfc9863c37caad3a06c4d42bfe4516
moniakteriitju <moniju582@gmail.com>
2022-03-11 15:34:09 +0600

    Registration process completed - database connection andsuccessfully inserter data ( commited by moni)

---
+++ CEPP/src/main/java/web/cepp/Web/UserRegistrationServlet.java
@@ -0,0 +1,62 @@
+package web.cepp.Web;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import web.cepp.Dao.UserInfoDao;
+import web.cepp.Dao.UserRegistrationDao;
+
+import java.io.IOException;
+
+/**
+ * Servlet implementation class UserRegistrationServlet
+ */
+public class UserRegistrationServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserRegistrationServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		int id = Integer.parseInt(request.getParameter("id"));
+		String name = request.getParameter("name");
+		String address = request.getParameter("address");
+		String phone_number = request.getParameter("phone_number");
+		String email_id = request.getParameter("email_id");
+		
+		String gender = request.getParameter("gender");
+		String type = request.getParameter("type");
+		
+		String age = request.getParameter("age");
+	
+		
+		
+		
+		
+		
+		
+		UserRegistrationDao.insertQuery(id, name, address, phone_number, email_id, gender, type, age);
+		doGet(request, response);
+	}
+
+}
